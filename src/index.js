14549f5cdbb5d14df9799b89dd7ab7a70418bde9
James Hatfield <james.hatfield17@icloud.com>
2025-04-08 15:58:08 +0100

    Add query selector functions and pseudocode for next steps

--- src/index.js
+++ src/index.js
@@ -1,2 +1,26 @@
 import "./styles.css";
+import { searchSelect, submitBtn } from "./querySelector";
 
+// Responsible for functionality to search item/location
+submitBtn().addEventListener("click", () => {
+    const search = searchSelect();
+
+    
+})
+
+
+/* 
+
+1. user types location
+
+2. Feed into the app div
+
+3. integrate with API, pull weather
+
+4. Pull weather data into app divs
+
+5. Put some placeholder data there
+
+6. Put some API handling functions in
+
+*/ 
