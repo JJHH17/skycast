49d902afb37b37baae744e86cf867d0cdd3bef94
James Hatfield <james.hatfield17@icloud.com>
2025-04-13 10:40:14 +0100

    Import chart.js into seperate file

--- src/graph.js
+++ src/graph.js
@@ -1,15 +1,7 @@
-import "./styles.css";
-import { searchSelect, submitBtn, locationDiv, myGraph, graphHolder } from "./querySelector";
-import { getWeather } from "./locationSearch";
+// File responsible for handling graph logic
 import Chart from "chart.js/auto";
 
 
-// Responsible for functionality to search item/location
-submitBtn().addEventListener("click", () => {
-    const search = searchSelect();
-
-    getWeather(search.value);
-})
 
 const ctx = document.getElementById('myChart').getContext('2d');
 
@@ -38,19 +30,3 @@ const myChart = new Chart(ctx, {
       }
     }
   });
-
-/* 
-
-1. user types location
-
-2. Feed into the app div
-
-3. integrate with API, pull weather
-
-4. Pull weather data into app divs
-
-5. Put some placeholder data there
-
-6. Put some API handling functions in
-
-*/ 
